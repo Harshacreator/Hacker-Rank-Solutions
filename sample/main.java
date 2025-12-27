1647b577b720257bd8c0c18a57880f5790c1b52b
Harshacreator <harshasignin96@gmail.com>
2025-12-27 12:11:15 +0530

    sample push

---
+++ sample/main.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        int a = num.nextInt();
+        int b = num.nextInt();
+        int c = num.nextInt();
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+        
+        
+        
+        
+        
+    }
+}
